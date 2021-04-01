a82db016df6b86cf417ec2e84edeaa27af5c5b2b
Melanie-Lindgren-nti-johanneberg <74713763+Melanie-Lindgren-nti-johanneberg@users.noreply.github.com>
2021-04-01 11:23:09 +0200

    försöka fixa mobil

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function getScrollbarWidth() {
-    var scroll = window.innerWidth - document.documentElement.clientWidth;
-    if (scroll == 17) {
-        document.getElementsByClassName('dum_header').style.marginRight = '17px';
-    } else {
-        document.getElementsByClassName('dum_header').style.marginRight = '0px';
-    }
-}
-// return window.innerWidth - document.documentElement.clientWidth;
