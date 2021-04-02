80074b8463f13d075aa7935986cff300c13db44c
Melanie-Lindgren-nti-johanneberg <74713763+Melanie-Lindgren-nti-johanneberg@users.noreply.github.com>
2021-04-02 18:23:59 +0200

    små fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+//Mobil - Meny för alla sidor
+document.querySelector(".menu-button").addEventListener("click",openNav)
+
+document.querySelector(".exitbutton").addEventListener("click",closeNav)
+
+function openNav() {
+    document.getElementById("mobile_nav").style.left = "0vw";
+}
+function closeNav() {
+    document.getElementById("mobile_nav").style.left = "-120vw";
+}
+
+//Mobil - Filter meny
 document.querySelector(".menu-button").addEventListener("click",openNav)
 
 document.querySelector(".exitbutton").addEventListener("click",closeNav)
