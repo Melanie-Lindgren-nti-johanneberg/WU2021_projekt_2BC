e4f91f4a8f2c15198f3a83e2622e8623a3d8234c
Melanie-Lindgren-nti-johanneberg <74713763+Melanie-Lindgren-nti-johanneberg@users.noreply.github.com>
2021-04-05 01:34:05 +0200

    klar med allt

--- js/main.js
+++ js/main.js
@@ -23,20 +23,3 @@ function myFunction() {
         document.querySelector(".visa_filter_button").innerHTML = "Dölj filter";
     }
 }
-
-//Datum indexsida
-
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = yyyy + '-' + mm + '-' + dd;
-
-// let today = new Date().toISOString().slice(0, 10);
-
-// console.log(today);
-
-document.querySelector(".date").innerHTML = today;
-// today = mm + '/' + dd + '/' + yyyy;
-// document.write(today);
